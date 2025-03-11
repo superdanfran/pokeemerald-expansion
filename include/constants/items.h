e1d0f9edb36877f6c41ce37a55f4c4181079ac46
Dani Tyler <dftyler@protonmail.com>
2025-03-11 13:27:11 +0000

    Add hyper candy

--- include/constants/items.h
+++ include/constants/items.h
@@ -995,11 +995,12 @@
 #define ITEM_STRANGE_BALL 828
 #define ITEM_ENDLESS_CANDY 829
 #define ITEM_INFINITE_REPEL 830
+#define ITEM_HYPER_CANDY 831
 
 // HOPO BERRY
 // LEGEND PLATE
 
-#define ITEMS_COUNT 831
+#define ITEMS_COUNT 832
 #define ITEM_FIELD_ARROW ITEMS_COUNT
 
 // A special item id associated with "Cancel"/"Exit" etc. in a list of items or decorations
