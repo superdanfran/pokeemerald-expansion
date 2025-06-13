3885c39141073594b7cb0e2bd123ba40169ec09e
Dani Tyler <dftyler@protonmail.com>
2025-06-13 11:58:15 +0100

    misc changes to split 1 and balance adjustments

--- src/data/pokemon/species_info/gen_4_families.h
+++ src/data/pokemon/species_info/gen_4_families.h
@@ -2363,7 +2363,7 @@ const struct SpeciesInfo gSpeciesInfoGen4[] =
         .tmIlliterate = TRUE,
         .levelUpLearnset = sCombeeLevelUpLearnset,
         .teachableLearnset = sCombeeTeachableLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 21, SPECIES_VESPIQUEN, CONDITIONS({IF_GENDER, MON_FEMALE})}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 21, SPECIES_VESPIQUEN}),
     },
 
     [SPECIES_VESPIQUEN] =
