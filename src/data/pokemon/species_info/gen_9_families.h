81e4d129080c4791367917e085a41bc24ee064d0
Dani Tyler <dftyler@protonmail.com>
2025-06-15 19:06:33 +0100

    Early game balance changes (including removing early acro!!!)

--- src/data/pokemon/species_info/gen_9_families.h
+++ src/data/pokemon/species_info/gen_9_families.h
@@ -1149,7 +1149,7 @@ const struct SpeciesInfo gSpeciesInfoGen9[] =
         )
         .levelUpLearnset = sPawmoLevelUpLearnset,
         .teachableLearnset = sPawmoTeachableLearnset,
-        .evolutions = EVOLUTION({EVO_OVERWORLD_STEPS, 1000, SPECIES_PAWMOT}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 32, SPECIES_PAWMOT}),
     },
 
     [SPECIES_PAWMOT] =
