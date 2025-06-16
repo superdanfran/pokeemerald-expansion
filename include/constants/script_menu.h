da99e59c5f4c7e000a8abc8cad5658ed0a242b0a
Dani Tyler <dftyler@protonmail.com>
2025-06-16 19:34:48 +0100

    Further work on starter gifts (Kalos and Alola)

--- include/constants/script_menu.h
+++ include/constants/script_menu.h
@@ -123,6 +123,9 @@
 #define MULTI_FALLARBOR_TENT_RULES         112
 #define MULTI_TAG_MATCH_TYPE               113
 #define MULTI_BERRY_PLOT                   114
+#define MULTI_KALOS_STARTER                115
+#define MULTI_ALOLA_STARTER                116
+#define MULTI_GALAR_STARTER                117
 
 // Lilycove SS Tidal Multichoice Selections
 #define SSTIDAL_SELECTION_SLATEPORT        0
