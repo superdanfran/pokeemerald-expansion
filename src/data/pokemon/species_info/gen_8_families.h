ac2907f2b3ccecb1b3d98603674e8723fcf56d49
Dani Tyler <dftyler@protonmail.com>
2025-06-29 12:04:42 +0100

    Change Yamper and Skwovet to evolve at level 21

--- src/data/pokemon/species_info/gen_8_families.h
+++ src/data/pokemon/species_info/gen_8_families.h
@@ -836,7 +836,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .levelUpLearnset = sSkwovetLevelUpLearnset,
         .teachableLearnset = sSkwovetTeachableLearnset,
         .eggMoveLearnset = sSkwovetEggMoveLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 24, SPECIES_GREEDENT}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 21, SPECIES_GREEDENT}),
     },
 
     [SPECIES_GREEDENT] =
@@ -2089,7 +2089,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .levelUpLearnset = sYamperLevelUpLearnset,
         .teachableLearnset = sYamperTeachableLearnset,
         .eggMoveLearnset = sYamperEggMoveLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 25, SPECIES_BOLTUND}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 21, SPECIES_BOLTUND}),
     },
 
     [SPECIES_BOLTUND] =
