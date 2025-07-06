0b79761b3f8a5f3d611ebf3475cc00b63e5c4b7c
Dani Tyler <dftyler@protonmail.com>
2025-07-06 19:38:26 +0100

    Add trainers to Jagged Path and Lavaridge Gym

--- src/caps.c
+++ src/caps.c
@@ -17,7 +17,7 @@ u32 GetCurrentLevelCap(void)
         {FLAG_DEFEATED_WALLY_MAUVILLE, 28},
         {FLAG_BADGE03_GET, 32},
         {FLAG_DEFEATED_EVIL_TEAM_MT_CHIMNEY, 38},
-        {FLAG_BADGE04_GET, 40},
+        {FLAG_BADGE04_GET, 45},
         {FLAG_BADGE05_GET, 55},
         {FLAG_BADGE06_GET, 66},
         {FLAG_BADGE07_GET, 77},
