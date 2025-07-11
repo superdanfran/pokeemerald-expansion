e9f85bcbe332f4d59ccb1d24f884ea740aac8990
Dani Tyler <dftyler@protonmail.com>
2025-07-11 12:50:47 +0100

    Add Norman gym trainers and leader fight

--- src/caps.c
+++ src/caps.c
@@ -18,7 +18,7 @@ u32 GetCurrentLevelCap(void)
         {FLAG_BADGE03_GET, 32},
         {FLAG_DEFEATED_EVIL_TEAM_MT_CHIMNEY, 38},
         {FLAG_BADGE04_GET, 45},
-        {FLAG_BADGE05_GET, 55},
+        {FLAG_BADGE05_GET, 50},
         {FLAG_BADGE06_GET, 66},
         {FLAG_BADGE07_GET, 77},
         {FLAG_BADGE08_GET, 88},
