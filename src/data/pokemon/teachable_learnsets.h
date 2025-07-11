6594f5dbe70a362129d7e6e551898d55767986a7
Dani Tyler <dftyler@protonmail.com>
2025-07-11 15:58:48 +0100

    Add fossils to desert underpass

--- src/data/pokemon/teachable_learnsets.h
+++ src/data/pokemon/teachable_learnsets.h
@@ -31667,7 +31667,6 @@ static const u16 sSobbleTeachableLearnset[] = {
     MOVE_SWIFT,
     MOVE_WATERFALL,
     MOVE_WATER_PULSE,
-    MOVE_BULLET_SEED,
     MOVE_UNAVAILABLE,
 };
 
@@ -31690,7 +31689,6 @@ static const u16 sDrizzileTeachableLearnset[] = {
     MOVE_SWIFT,
     MOVE_WATERFALL,
     MOVE_WATER_PULSE,
-    MOVE_BULLET_SEED,
     MOVE_UNAVAILABLE,
 };
 
@@ -31722,7 +31720,6 @@ static const u16 sInteleonTeachableLearnset[] = {
     MOVE_TAUNT,
     MOVE_WATERFALL,
     MOVE_WATER_PULSE,
-    MOVE_BULLET_SEED,
     MOVE_UNAVAILABLE,
 };
 #endif //P_FAMILY_SOBBLE
